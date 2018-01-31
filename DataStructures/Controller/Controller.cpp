9ddd9764a641a72582527d87f01e705f0b81700a
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2018-01-31 11:34:36 -0700

    Created a data structures project that is very important.

---
+++ DataStructures/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  DataStructures
+//
+//  Created by Pulsipher, Noah on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ DataStructures/Controller/Controller.hpp
@@ -0,0 +1,14 @@
+//
+//  Controller.hpp
+//  DataStructures
+//
+//  Created by Pulsipher, Noah on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <stdio.h>
+
+#endif /* Controller_hpp */
