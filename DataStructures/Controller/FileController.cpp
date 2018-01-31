8093bf21235577dd2ce032acb90bfec9430b10ba
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2018-01-31 12:57:29 -0700

    Fixing an error I had with some of the files.

---
+++ DataStructures/Controller/FileController.cpp
@@ -0,0 +1,44 @@
+//
+//  FileController.cpp
+//  DataStructures
+//
+//  Created by Pulsipher, Noah on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "FileController.hpp"
+
+vector<CrimeData> FileController :: readCrimeDataToVector(string filename)
+{
+    std :: vector<CrimeData> crimeVector;
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(filename);
+    
+    //If the file exists at that path.
+    if (dataFile.is_open())
+    {
+        //Keep reading until you are at the end of the file.
+        while (!dataFile.eof())
+        {
+            //Grab each line from the CSV separated by the carriage return character.
+            getline(dataFile, currentCSVLine, '\r');
+            //Exclude header row.
+            if (rowCount != 0)
+            {
+                //Create a CrimeData instance from the line.
+                CrimeData row(currentCSVLine);
+                crimeVector.push_back(row);
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    
+    return crimeVector;
+}
---
+++ DataStructures/Controller/FileController.hpp
@@ -0,0 +1,27 @@
+//
+//  FileController.hpp
+//  DataStructures
+//
+//  Created by Pulsipher, Noah on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include "../Data/CrimeData.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class FileController
+{
+public:
+    static vector<CrimeData> readCrimeDataToVector(string filename);
+};
+
+#endif /* FileController_hpp */
