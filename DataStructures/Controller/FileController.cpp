49fce59b5bc1d927930338ff0369c76622c20e3a
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2018-01-31 13:12:23 -0700

    added an if block to catch empty line.

--- DataStructures/Controller/FileController.cpp
+++ DataStructures/Controller/FileController.cpp
@@ -28,8 +28,11 @@ vector<CrimeData> FileController :: readCrimeDataToVector(string filename)
             if (rowCount != 0)
             {
                 //Create a CrimeData instance from the line.
+                if(currentCSVLine.length() != 0)
+                {
                 CrimeData row(currentCSVLine);
                 crimeVector.push_back(row);
+                }
             }
             rowCount++;
         }
