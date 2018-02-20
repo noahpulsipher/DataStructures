ca4897fe7459a3088c9b548eae387a7b867d5ee2
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2018-02-20 11:56:29 -0700

    adding the addAtIndex method.

--- DataStructures/Model/Structures/LinkedList.hpp
+++ DataStructures/Model/Structures/LinkedList.hpp
@@ -57,4 +57,54 @@ LinkedList<Type> :: ~LinkedList()
     }
 }
 
+void LinkedList<Type> :: add(Type item)
+{
+    LinearNode<Type> * newData = new LinearNode<Type>(item);
+    
+    if(this->size == 0)
+    {
+        this->front = newData;
+    }
+    else
+    {
+        this->end->setNextNode(newData);
+    }
+    
+    this->end = newData;
+    
+    this->size += 1;
+}
+
+void LinkedList<Type> :: addAtIndex(int index, Type item)
+{
+    assert(index >= 0 && index <= this->size);
+    if(index == this->size)
+    {
+        add(item);
+    }
+    else
+    {
+        LinearNode<Type> * toBeAdded = new LinearNode<Type>(item);
+        if (index == 0)
+        {
+            toBeAdded->setNextNode(front);
+            front = toBeAdded;
+        }
+        else
+        {
+            LinearNode<Type> * previous = nullptr;
+            LinearNode<Type> * current = front;
+            for (int position = 0; position < index; position++)
+            {
+                previous = current;
+                current = current->getNextNode();
+            }
+            previous->setNextNode(toBeAddad);
+            toBeAdded->setNextNode(current);
+        }
+        this->size++;
+    }
+}
+}
+
 #endif /* LinkedList_hpp */
