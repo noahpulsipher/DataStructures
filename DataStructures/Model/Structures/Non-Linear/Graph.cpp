23b2c9bacd3973477e57d52bf8e35d97ed52c3b4
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2018-03-27 13:54:12 -0600

    added the tree file.

--- DataStructures/Model/Structures/Non-Linear/Graph.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Graph.cpp
-//  DataStructures
-//
-//  Created by Pulsipher, Noah on 3/19/18.
-//  Copyright © 2018 CTEC. All rights reserved.
-//
-
-#include "Graph.hpp"
---
+++ DataStructures/Model/Structures/Non-Linear/Tree.hpp
@@ -0,0 +1,37 @@
+//
+//  Tree.hpp
+//  DataStructures
+//
+//  Created by Pulsipher, Noah on 3/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Tree_hpp
+#define Tree_hpp
+
+#include "/Users/npul1355/Documents/C++/DataStructures/DataStructures/Model/Nodes/BinaryTreeNode.hpp"
+
+template <class Type>
+class Tree
+{
+protected:
+    BinaryTreeNode<Type> * root;
+public:
+    //Informational Methods
+    virtual int getSize() = 0;
+    virtual int getHeight() = 0;
+    virtual bool isComplete() = 0;
+    virtual bool isBalanced() = 0;
+        
+    //*Data Methods*
+    virtual void insert(Type data) = 0;
+    virtual bool contains(Type data) = 0;
+    virtual void remove(Type data) = 0;
+    
+    //*Traversals*
+    virtual void inOrderTraversal() = 0;
+    virtual void preOrderTraversal() = 0;
+    virtual void postOrderTraversal() = 0;
+};
+
+#endif /* Tree_hpp */
