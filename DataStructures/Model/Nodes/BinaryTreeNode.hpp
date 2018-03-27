9303f9ce1b72e25b9addaa9ce067dc09e6a649a0
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2018-03-27 12:04:54 -0600

    Added the binary tree node files to the group folder and made an umlet.jar file.

---
+++ DataStructures/Model/Nodes/BinaryTreeNode.hpp
@@ -0,0 +1,14 @@
+//
+//  BinaryTreeNode.hpp
+//  DataStructures
+//
+//  Created by Pulsipher, Noah on 3/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeNode_hpp
+#define BinaryTreeNode_hpp
+
+#include <stdio.h>
+
+#endif /* BinaryTreeNode_hpp */
