3261505e805d19903ed895b9273260fcbfd053d7
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2018-04-27 12:43:02 -0600

    adding stubs to hashtable file

--- DataStructures/Model/Nodes/HashNode.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  HashNode.cpp
-//  DataStructures
-//
-//  Created by Pulsipher, Noah on 4/27/18.
-//  Copyright © 2018 CTEC. All rights reserved.
-//
-
-#include "HashNode.hpp"
---
+++ DataStructures/Model/Structures/Non-Linear/Hashtable.hpp
@@ -0,0 +1,94 @@
+//
+//  Hashtable.hpp
+//  DataStructures
+//
+//  Created by Pulsipher, Noah on 4/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Hashtable_hpp
+#define Hashtable_hpp
+
+#include "/Users/npul1355/Documents/C++/DataStructures/DataStructures/Model/Nodes/HashNode.hpp"
+
+template <class Type>
+class Hashtable
+{
+private:
+    HashNode<Type> ** internalStorage;
+    long size;
+    long capacity;
+    double loadFactor;
+    
+    long getNextPrime();
+    bool isPrime(long current);
+    void resize();
+    long findPosition(HashNode<Type>* insertedNode);
+    long handleCollision(HashNode<Type>* insertedNode, long index);
+    
+public:
+    Hashtable();
+    ~Hashtable();
+    
+    void insert(Type data);
+    long getSize();
+};
+
+template <class Type>
+Hashtable<Type> :: Hashtable()
+{
+    capacity = 101;
+    internalStorage = new HashNode<Type>*[capacity];
+    size = 0;
+    loadFactor = .80;
+}
+
+template <class Type>
+Hashtable<Type> :: ~HashTable()
+{
+    delete [] internalStorage;
+}
+
+template <class Type>
+bool Hashtable<Type> :: isPrime(long current)
+{
+    return false;
+}
+
+template <class Type>
+long Hashtable<Type> :: findPosition(HashNode<Type> * insert)
+{
+    return -1;
+}
+
+template <class Type>
+long Hashtable<Type> :: handleCollision(HashNode<Type> * current, long index)
+{
+    return -1;
+}
+
+template <class Type>
+long Hashtable<Type> :: getSize()
+{
+    return -1;
+}
+
+template <class Type>
+long Hashtable<Type> :: getNextPrime()
+{
+    return -1;
+}
+
+template <class Type>
+void Hashtable<Type> :: resize()
+{
+    
+}
+
+template <class Type>
+void Hashtable<Type> :: insert(Type value)
+{
+    
+}
+
+#endif /* Hashtable_hpp */
